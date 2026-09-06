811d351e185c3936619c6a3b6949005cbd19f83d
noirdotdeb <shykhalisaadattique@gmail.com>
2026-09-06 21:52:24 +0500

    chore: scaffold main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+import { createElement } from './core/createElement.js';
+import { render } from './core/render.js';
+
+// TODO: call createElement to describe an <h1> that says "Hello, Nucleus"
+// TODO: call render to put it inside document.getElementById('app')
